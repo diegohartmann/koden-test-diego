f54c8264ad4afc368cd79dc17ffbf2f86e7cb82d
Diego Hartmann <contato.hartmann8@gmail.com>
2021-08-04 18:52:01 -0300

    pag afiliado quase concluida, falta segunda sessao

---
+++ files/js/vantagens-afiliado-dinamicly-creator.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    console.log(window.location.href);
+    LoadServicesDynamicaly(`${window.location.href}/../js/jsons/vantagens-afiliado.json`);
+});
+function LoadServicesDynamicaly (_jsonPath) {
+    const itemsContainer = $('.vantagens-itens');
+    $.getJSON(_jsonPath, function(data){
+        data.forEach(service => {
+            appendService(itemsContainer, service);
+        });
+    }).fail(function(){
+        console.log("Erro ao ler arquivo .json de 'vantagens'")
+        appendService(itemsContainer, "Erro. Recarregue a página.");
+    });
+}
+function appendService(_container, _service){
+    _container.append( getServiceHTML(_service) )
+}
+const getServiceHTML = (__service) =>{
+    return `
+        <div class="item-single">
+            <img src="assets/img/eumei-list-style.png" alt="vantagem">
+            <p>${__service}</p>
+        </div>
+    `
+}
