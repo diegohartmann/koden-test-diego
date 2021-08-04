b032f35c185a57a9cd203dd9c9b63a1d96f6975f
Diego Hartmann <contato.hartmann8@gmail.com>
2021-08-04 10:43:58 -0300

    footer concluido

--- README.md
+++ README.md
@@ -18,8 +18,10 @@
 		(03/08)
 		1.1 Senti falta de usar React para reutilizar o botão verde do site;
 		1.2 Script que adiciona itens (serviços) dinamicamente atravez de um arquivo .json contendo o array com todos eles (servicos.json);
-		1.3 Criação da sessão de planos disponíveis, dinamicamente com json
-		1.4 Término de todas as sessões da home page, apenas faltando o footer
+		1.3 Criação da sessão de planos disponíveis, dinamicamente com json;
+		1.4 Término de todas as sessões da home page, apenas faltando o footer;
 		(04/08)
+		1.5 Finalização do footer da home;
+		1.6 Testei site em resolucao 1920 em :http://whatismyscreenresolution.net/;
 	
 	2. Criação da pagina de afiliado
--- files/js/planos-dinamicaly-creator.js
+++ files/js/planos-dinamicaly-creator.js
@@ -5,14 +5,14 @@ function LoadItems (_jsonPath) {
     const itemsContainer = $('.planos-container');
     $.getJSON(_jsonPath, function(data){
         data.forEach(planoSingle => {
-            appendInContainer(itemsContainer, planoSingle);
+            appendPlan(itemsContainer, planoSingle);
         });
     }).fail(function(){
-        console.log("Erro ao ler arquivo .json de 'serviços'")
-        appendInContainer(itemsContainer, "Erro. Recarregue a página.");
+        console.log("Erro ao ler arquivo .json de 'planos'")
+        appendPlan(itemsContainer, "Erro. Recarregue a página.");
     });
 }
-function appendInContainer(_container, _planoSingle){
+function appendPlan(_container, _planoSingle){
     _container.append(getPlanoSingleHTML(_planoSingle.price, _planoSingle.about, _planoSingle.planNum, getItemsHTML(_planoSingle.items)));
 }
 function getPlanoSingleHTML(_price, _about, _planNum, _getItemsHTML){
